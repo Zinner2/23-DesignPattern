3789de6ca96eb012a4dc3e1b2a82fd0af98ae1b0
任人子 <486084544@qq.com>
2021-09-20 15:22:30 +0800

    2021.9.20 张俊杰

--- 01-Singleton Pattern/src/com/jj/hungryman/HungryManTest.java
+++ 01-Singleton Pattern/src/com/jj/hungryman/HungryManTest.java
@@ -6,6 +6,14 @@ package com.jj.hungryman;
  */
 public class HungryManTest {
     public static void main(String[] args) {
+        Singleton singleton1 = Singleton.getSingleton();
+        Singleton singleton2 = Singleton.getSingleton();
 
+        /*
+            singleton1 = 1360875712
+            singleton2 = 1360875712
+        */
+        System.out.println("singleton1 = " + singleton1.hashCode());
+        System.out.println("singleton2 = " + singleton2.hashCode());
     }
 }
